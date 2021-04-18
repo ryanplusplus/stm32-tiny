7443c306ea74d07fca01dbfe5bb0962da25864d3
Ryan Hartlage <ryanplusplus@gmail.com>
2021-04-17 20:44:06 -0400

    Added blue-pill-plus target

---
+++ blue-pill-plus.mk
@@ -0,0 +1,42 @@
+TARGET = $(subst .mk,,$(firstword $(MAKEFILE_LIST)))
+BUILD_DIR ?= ./build/$(TARGET)
+
+CPU := cortex-m3
+ARCH := armv7-m
+LINKER_CFG := ld/$(TARGET).ld
+SVD := svd/stm32f103.svd
+
+DEBUG_ADAPTER ?= openocd
+OPENOCD_CFG_DIR := openocd/$(TARGET)
+BLACK_MAGIC_PORT ?= /dev/ttyACM0
+BLACK_MAGIC_POWER_TARGET ?= N
+
+DEFINES := \
+  STM32F103xB \
+  HSE_VALUE=8000000 \
+
+include tools/defaults.mk
+
+CPPFLAGS += \
+  -Wno-unused-parameter \
+
+SRC_DIRS := \
+  src/peripheral/cmsis \
+  src/peripheral/stm32xxxx \
+  src/target/$(TARGET) \
+
+SRC_FILES := \
+  lib/stm32cube/CMSIS/STM32F1xx/src/system_stm32f1xx.c \
+
+INC_DIRS := \
+  lib/stm32cube/CMSIS/ARM/inc \
+  lib/stm32cube/CMSIS/STM32F1xx/inc \
+  lib/stm32cube/HAL/STM32F1xx/inc \
+
+include lib_tiny.mk
+
+include tools/tools.mk
+
+.PHONY: watch
+watch:
+	@rerun "$(MAKE) --no-print-directory -f $(firstword $(MAKEFILE_LIST))"
